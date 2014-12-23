e9ae1ca1cddf64c0c20850fb636f733acbcca8b9
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2014-12-23 22:23:35 +0300

    Initial commit

---
+++ src/DataDownloaderService/WoWDataDownloader/WoWDataDownloader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WoWDataDownloader
+//
+//  Created by Anton Bogushevsky on 23.12.14.
+//  Copyright (c) 2014 Anton Bogushevsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
