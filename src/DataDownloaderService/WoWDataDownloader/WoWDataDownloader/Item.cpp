ea3dd42c696a9d2cb38597214c82e04dce58e5d5
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2014-12-24 22:01:21 +0300

    Entity class added

---
+++ src/DataDownloaderService/WoWDataDownloader/WoWDataDownloader/Item.cpp
@@ -0,0 +1,9 @@
+//
+//  Item.cpp
+//  WoWDataDownloader
+//
+//  Created by Anton Bogushevsky on 24.12.14.
+//  Copyright (c) 2014 Anton Bogushevsky. All rights reserved.
+//
+
+#include "Item.h"
---
+++ src/DataDownloaderService/WoWDataDownloader/WoWDataDownloader/Item.h
@@ -0,0 +1,18 @@
+//
+//  Item.h
+//  WoWDataDownloader
+//
+//  Created by Anton Bogushevsky on 24.12.14.
+//  Copyright (c) 2014 Anton Bogushevsky. All rights reserved.
+//
+
+#ifndef __WoWDataDownloader__Item__
+#define __WoWDataDownloader__Item__
+
+#include <stdio.h>
+
+class Entity {
+    
+};
+
+#endif /* defined(__WoWDataDownloader__Item__) */
