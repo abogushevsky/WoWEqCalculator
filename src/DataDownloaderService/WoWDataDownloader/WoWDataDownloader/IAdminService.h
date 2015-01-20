93ddb84c352878d7b174c80720f40fc9a2e7b710
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2015-01-20 21:30:32 +0300

    IAdminService

---
+++ src/DataDownloaderService/WoWDataDownloader/WoWDataDownloader/IAdminService.h
@@ -0,0 +1,17 @@
+//
+//  IAdminService.h
+//  WoWDataDownloader
+//
+//  Created by Anton Bogushevsky on 20.01.15.
+//  Copyright (c) 2015 Anton Bogushevsky. All rights reserved.
+//
+
+#ifndef WoWDataDownloader_IAdminService_h
+#define WoWDataDownloader_IAdminService_h
+
+class IAdminService {
+public:
+    virtual ~IAdminService() = 0;
+};
+
+#endif
